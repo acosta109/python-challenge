46c6ccab32123915cc7a302c57ede606423f2f13
Anthony Costa <acosta1019@gmail.com>
2023-01-12 16:07:56 -0500

    Finished PyBank part of the assignment. Uploading completed code and txt doc.

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,6 +11,7 @@ with open(csvpath) as csvfile:
     totalProfit = 0
     totalMonths = 0
     deltaArray = []
+    dateArray = []
     avgDelta = 0
     #skip header row 
     next(csvreader)
@@ -18,16 +19,25 @@ with open(csvpath) as csvfile:
     for rows in csvreader:
         totalProfit += int(rows[1])
         totalMonths +=1
+        
+        #the first time we go get a change in price, track it
 
         #track change in profit int(rows[1]) is the current month's profit 
         # and lastProfit is last months profit
         if totalMonths > 1:
             deltaArray.append(int(rows[1]) - lastProfit)
+            dateArray.append(str(rows[0]))
 
         lastProfit = int(rows[1])    
 
     avgDelta = round(sum(deltaArray)/len(deltaArray), 2)
+    greatestIncIndex = deltaArray.index(max(deltaArray))
+    greatestDecIndex = deltaArray.index(min(deltaArray))
 
+    incDate = dateArray[greatestIncIndex]
+    decDate = dateArray[greatestDecIndex]
+
+    
     #print(avgDelta) #debugging code
     #print(lastProfit) debugging code
     #print(totalProfit) #debugging code 
@@ -40,5 +50,14 @@ with open(csvpath) as csvfile:
     print(f'Total Months: {totalMonths}\n')
     print(f'Total: ${totalProfit}\n')
     print(f'Average Change: ${avgDelta}\n')
-    print(f'Greatest Increase in Profits: {max(deltaArray)}\n')
-    print(f'Greatest Decrease in Profits: {min(deltaArray)}\n')
+    print(f'Greatest Increase in Profits: {incDate} (${max(deltaArray)})\n')
+    print(f'Greatest Decrease in Profits: {decDate} (${min(deltaArray)})\n')
+
+with open('financial_results.txt', 'w') as file:
+    file.write("Financial Analysis \n")
+    file.write("---------------------------- \n")
+    file.write(f'Total Months: {totalMonths}\n')
+    file.write(f'Total: ${totalProfit}\n')
+    file.write(f'Average Change: ${avgDelta}\n')
+    file.write(f'Greatest Increase in Profits: {incDate} (${max(deltaArray)})\n')
+    file.write(f'Greatest Decrease in Profits: {decDate} (${min(deltaArray)})\n')
